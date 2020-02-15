0abc6de6858a8b3acd6e844247f97adba3d0bb6a
Christian Hughes <xdotdash@gmail.com>
2020-02-15 17:35:15 -0600

    Split up optional into optional and optionalOrElse

--- director-core/src/main/kotlin/pw/dotdash/director/core/parameter/OptionalParameter.kt
+++ director-core/src/main/kotlin/pw/dotdash/director/core/parameter/OptionalParameter.kt
@@ -9,17 +9,16 @@ import pw.dotdash.director.core.lexer.CommandTokens
 import pw.dotdash.director.core.value.ValueParameter
 
 @JvmOverloads
-fun <S, P : HList<P>, V : Any> ValueParameter<S, P, V>.optional(default: (() -> V)? = null, weak: Boolean = false): ValueParameter<S, P, V?> =
-    OptionalParameter(this, default, weak)
+fun <S, P : HList<P>, V : Any> ValueParameter<S, P, V>.optional(weak: Boolean = false): ValueParameter<S, P, V?> =
+    OptionalParameter(this, weak)
 
 private data class OptionalParameter<S, P : HList<P>, V : Any>(
     val parameter: ValueParameter<S, P, V>,
-    val default: (() -> V)?,
     val weak: Boolean
 ) : ValueParameter<S, P, V?> {
 
     override fun parse(source: S, tokens: CommandTokens, previous: P): V? {
-        if (!tokens.hasNext()) return this.default?.invoke()
+        if (!tokens.hasNext()) return null
 
         val snapshot: CommandTokens.Snapshot = tokens.snapshot
 
@@ -28,7 +27,37 @@ private data class OptionalParameter<S, P : HList<P>, V : Any>(
         } catch (e: ArgumentParseException) {
             if (this.weak || tokens.hasNext()) {
                 tokens.snapshot = snapshot
-                return this.default?.invoke()
+                return null
+            }
+            throw e
+        }
+    }
+
+    override fun getUsage(source: S, key: String): String =
+        "[${this.parameter.getUsage(source, key)}]"
+}
+
+@JvmOverloads
+fun <S, P : HList<P>, V : Any> ValueParameter<S, P, V>.optionalOrElse(weak: Boolean = false, orElse: () -> V): ValueParameter<S, P, V> =
+    OptionalOrElseParameter(this, weak, orElse)
+
+private data class OptionalOrElseParameter<S, P : HList<P>, V : Any>(
+    val parameter: ValueParameter<S, P, V>,
+    val weak: Boolean,
+    val default: (() -> V)
+) : ValueParameter<S, P, V> {
+
+    override fun parse(source: S, tokens: CommandTokens, previous: P): V {
+        if (!tokens.hasNext()) return this.default.invoke()
+
+        val snapshot: CommandTokens.Snapshot = tokens.snapshot
+
+        try {
+            return this.parameter.parse(source, tokens, previous)
+        } catch (e: ArgumentParseException) {
+            if (this.weak || tokens.hasNext()) {
+                tokens.snapshot = snapshot
+                return this.default.invoke()
             }
             throw e
         }
